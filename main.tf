899a48e4c701c00ee63d8fa7f660f8d0bf982c06
JefferyChenjy <jefferychenjy@users.noreply.github.com>
2026-09-23 22:30:02 +0800

    add checkov missing files as comments in main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,16 @@ provider "aws" {
 
 resource "aws_s3_bucket" "workshop" {
   bucket_prefix = "jeffery-module3.2"
+
+  #checkov:skip=CKV2_AWS_61: "Ensure that an S3 bucket has a lifecycle configuration"
+  #checkov:skip=CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
+  #checkov:skip=CKV2_AWS_62: "Ensure S3 buckets should have event notifications enabled"
+  #checkov:skip=CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
+  #checkov:skip=CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
+  #checkov:skip=CKV_AWS_145: "Ensure that S3 buckets are encrypted with KMS by default"
+  #checkov:skip=CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled"
 }
+
 
 terraform {
   required_providers {
@@ -26,3 +35,4 @@ terraform {
 terraform {
   required_version = ">= 1.0" 
 }
+
