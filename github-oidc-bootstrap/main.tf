7b2e1bcbbf7450a2bbc30294386d5be6db15065e
JefferyChenjy <jefferychenjy@users.noreply.github.com>
2026-09-23 22:20:15 +0800

    terraform required version and required provider

--- github-oidc-bootstrap/main.tf
+++ github-oidc-bootstrap/main.tf
@@ -53,11 +53,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.66.0"
+      version = "~> 2"
     }
   }
 }
 
 terraform {
-  required_version = ">= 1.0.0" 
+  required_version = ">= 1.0" 
 }
